fadb3ab822cf0569e6bd0b895a17b0f43a13b630
Maheen Unzeelah <46377344+maheenunzeelah@users.noreply.github.com>
2020-07-26 23:00:38 +0500

    css

--- src/components/PublicPages/card.js
+++ src/components/PublicPages/card.js
@@ -5,10 +5,11 @@ import {BrowserRouter, Route, Switch , withRouter} from 'react-router-dom';
 
 function Card(){
 return(
-    <div className="card" style={{backgroundColor:"#1a6365" , color:"rgb(183, 240, 240"}}>
+    <div className="card" style={{backgroundColor:"rgb(1, 4, 20)" , color:"rgb(183, 240, 240"}}>
      <div className="row">
+     <div className="col-lg-1"></div>
      <div className="col-lg-4">
-     <div className="list-group" style={{width:'300px' , margin:"20px auto 20px 30px"}} >
+     <div className="list-group" style={{width:'300px' , margin:"20px auto 20px 30px" ,backgroundColor:'#bbdefb'}} >
         <button type="button" class="list-group-item list-group-item-primary list-group-item-action active" >
          <a className="navbar-brand" href="/tourpage" style={{color:"white"}}>Overview</a>
         </button>
@@ -19,7 +20,7 @@ return(
         <button type="button" className="list-group-item list-group-item-success list-group-item-action">Customers</button>
     </div>
     </div>
-    <div className="col-lg-8" style={{marginTop:"20px"}}>
+    <div className="col-lg-7" style={{marginTop:"20px"}}>
     <Video />
     </div>
     </div>
