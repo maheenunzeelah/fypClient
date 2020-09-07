87e3b1baebf3182bbcecca1b48aaa33c7666eae1
Maheen Unzeelah <46377344+maheenunzeelah@users.noreply.github.com>
2020-09-07 23:05:10 +0500

    deploy

--- src/apis/postDataApi.js
+++ src/apis/postDataApi.js
@@ -3,9 +3,10 @@ import { type } from 'os';
 const formData = new FormData();
  
 export default axios.create({
-    baseURL:'http://localhost:3001',
+    baseURL:'https://infinite-eyrie-31819.herokuapp.com/',
     headers:{
         authorization:localStorage.getItem('jwtToken'),
   
     }
 })
+//http://localhost:3001
