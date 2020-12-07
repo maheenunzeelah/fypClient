59f32316df22d4728c8405ea9e9de19ebfcc1589
Maheen Unzeelah <46377344+maheenunzeelah@users.noreply.github.com>
2020-12-07 22:34:16 +0500

    quis

--- src/components/Student/quiz.js
+++ src/components/Student/quiz.js
@@ -23,7 +23,7 @@ const styles = (themes) => ({
   },
 
 })
-class Quiz extends Component {
+class Quiz extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
@@ -60,64 +60,93 @@ class Quiz extends Component {
   // handleClick=()=>{
 
   // }
+  // handleAnsClick=(value)=>{
+  //    this.props.reset();
+  //    const {currentQuesIndex,answers,answer}=this.state
+  //    const {questions}=this.props;
+  //    if(currentQuesIndex==questions.length-1){
+  //     this.setState(prevState => {
+  //             return prevState.perct = prevState.score * 100 / this.props.questions.length
+  //           }, () => this.props.result(this.state.answers, this.props.questions[index].test, this.state.score, this.state.perct)) 
+  //    }
+  //    else{
+  //      this.setState(prevState=>{
+  //        return {
+  //          currentQuesIndex:prevState.currentQuesIndex+1,
+  //          answers=[...prevState.answers,{...value,ques:this.props.questions[prevState.currentQuesIndex].question}],
+
+  //        }
+  //      })
+  //    }
+  // }
   handleAnsClick = (value) => {
     const { classes } = this.props
     //check if tests have questions or not
-    console.log(value)
+    let index;
+    let key;
+    let ans;
     this.props.reset()
+    console.log(this.state.currentQuesIndex)
     if (this.state.currentQuesIndex != this.props.questions.length) {
       this.setState((prevState) => {
+        console.log(prevState.currentQuesIndex, this.props.questions[prevState.currentQuesIndex].question)
         // increase index
-        return prevState.currentQuesIndex = prevState.currentQuesIndex + 1
+        return {
+          answers: [...prevState.answers, { ...value, ques: this.props.questions[prevState.currentQuesIndex].question }],
+          currentQuesIndex: prevState.currentQuesIndex + 1,
+          answer: [...prevState.answer, value]
+        }
+      }, () => {
+        console.log(this.state.answers)
+
       })
-    }
-    let index = this.state.currentQuesIndex
-    console.log(Object.values(value))
-    let key = Object.keys(value)
-    this.setState({
-      answers:[...this.state.answers,{...value,ques:this.props.questions[this.state.currentQuesIndex].question}]
-    },()=>{console.log(this.state.answers)})
-    // value = { ...{ ...value, ques: this.props.questions[this.state.currentQuesIndex].question } }
-    
-    let ans = Object.values(value) //to match T/F ans
-   //to match mcqs ans
-    
 
-    if (this.props.questions[index].type === 'T/F') {
-      if (ans[index] === this.props.questions[index].answer) {
-        this.setState(prevState => {
-          return prevState.score = prevState.score + 1
-        })
-        console.log(ans[index] + " is correct")
+      index = this.state.currentQuesIndex
+
+      console.log(index)
+      key = Object.keys(value) //to match mcqs ans
+
+      // value = { ...{ ...value, ques: this.props.questions[this.state.currentQuesIndex].question } }
+
+      ans = Object.values(value) //to match T/F ans
+      
+
+
+      if (this.props.questions[index].type === 'T/F') {
+
+        if (ans[0] == this.props.questions[index].answer) {
+          console.log('yes')
+          this.setState(prevState => {
+            return prevState.score = prevState.score + 1
+          }, () => console.log(ans[0] + " is correct"))
+
+        }
+        else {
+          console.log(ans[0] + " is incorrect")
+        }
       }
-      else {
-        console.log(ans[index] + " is incorrect")
+
+      else if (this.props.questions[index].type === 'MCQs') {
+        // The ans clicked by user will have true value
+
+        let score = this.props.questions[index].corr.reduce((acc, corrAns, i) => {
+          if (key.length != this.props.questions[index].corr.length)
+            return
+          if (key[i].indexOf(corrAns) != -1) {
+            return acc + 1
+          }
+        }, 0)
+        console.log("score" + score)
+        if (score == this.props.questions[index].corr.length)
+          this.setState(prevState => { return { score: prevState.score + 1 } })
       }
     }
 
-    else if (this.props.questions[index].type === 'MCQs') {
-      // The ans clicked by user will have true value
-      if (ans[index]) {
-        this.props.questions[index].corr.map(corrAns => {
-          //If corr ans array have name part of which matches with key of selected ans
-          if (key[index].indexOf(corrAns) != -1) {
-            this.setState(prevState => {
-              return prevState.score = prevState.score + 1
-            })
-            console.log(key[index] + " is correct")
-          }
-          else {
-            console.log(key[index] + " is incorrect")
-          }
-        })
-      }
-    }
     if (this.state.currentQuesIndex == this.props.questions.length - 1) {
-      console.log("done")
+      console.log(this.state.perct)
       this.setState(prevState => {
-        return prevState.perct = prevState.score * 100 / this.props.questions.length,
-          prevState.answer = value
-      }, () => this.props.result(this.state.answers, this.props.questions[index].test, this.state.score, this.state.perct)) 
+        return prevState.perct = prevState.score * 100 / this.props.questions.length
+      }, () => this.props.result(this.state.answers, this.props.questions[index].test, this.state.score, this.state.perct))
 
     }
   }
@@ -125,7 +154,7 @@ class Quiz extends Component {
   render() {
     const { classes, questions } = this.props
     let val, val2
-    console.log(questions.length, this.state.currentQuesIndex,this.state.answer)
+    console.log(questions.length, this.state.currentQuesIndex)
     if (!isEmpty(questions) && this.state.currentQuesIndex != questions.length) {
       return (
         <div className="container">
@@ -155,7 +184,7 @@ class Quiz extends Component {
                   </div> : <div>
                       {questions[this.state.currentQuesIndex].answers.map((ans, i) => {
                         return <div><label>
-                          <Field type="checkbox" name={`answer${i+1}`} component="input" style={{ margin: "10px", fontSize: '18px' }}  />
+                          <Field type="checkbox" name={`answer${i + 1}`}  normalize={v => v ? Object.values(ans)[0] : '0'} component="input" style={{ margin: "10px", fontSize: '18px' }} />
                           {Object.values(ans)}
 
                         </label>
@@ -176,6 +205,9 @@ class Quiz extends Component {
     }
     else if (this.state.currentQuesIndex == questions.length) {
       const { testName, groupName } = this.props.location.state;
+      const { answer } = this.state
+
+
       return <Card className="container" style={{ marginTop: '80px', padding: '50px', marginBottom: '80px', border: '3px solid #e3f2fd' }}>
         <CardContent>
           <div className="container">
@@ -202,9 +234,8 @@ class Quiz extends Component {
               <div className="col-md-9">
 
                 {questions.map((ques, index) => {
-                  val = Object.values(this.state.answer)
 
-                  console.log(val[index])
+                  console.log(answer)
 
                   return <div className=" jumbotron grey lighten-4">
                     <h4><bold>{`Question${index + 1} of ${questions.length}`}</bold></h4>
@@ -214,13 +245,26 @@ class Quiz extends Component {
 
                       {ques.type == 'T/F' ?
                         <div ><p className="green-text font-weight-bold">Correct Ans: {ques.answer}</p>
-                          <p className={ques.answer == val[index] ? "black-text" : "red-text"}> Your Ans : {val[index]}</p>
+                          <p className={ques.answer == Object.values(answer[index]) ? "black-text" : "red-text"}> Your Ans : {Object.values(answer[index])}</p>
                         </div>
-                        : <div><p>Correct Ans: {ques.corr.map(ans => <ul><li>{ques.answers.map(an => {
+                        : <div><p>Correct Ans: {ques.corr.map(ans => 
+                        <ul><li>{ques.answers.map(an => {
                           if (Object.keys(an) == ans) {
                             return Object.values(an)
                           }
-                        })}</li></ul>)}</p>
+                        })}
+                        </li></ul>
+                      )}</p>
+                     <p >Your Ans: 
+                     {
+                       ques.answers.map((ans,i)=>
+                      {
+                        if(Object.keys(answer[index])[i]==Object.keys(ans)){
+                          return <li>{Object.values(ans)[i]}</li>
+                        }
+                        
+                        }
+                      )}</p>
                         </div>
                       }
 
@@ -240,7 +284,7 @@ class Quiz extends Component {
   }
 }
 const mapStateToProps = (state) => {
-  console.log(state.questionList)
+ console.log(state.questionList)
   return {
     questions: state.questionList,
     user: state.auth.user
@@ -249,5 +293,5 @@ const mapStateToProps = (state) => {
 const formValues = reduxForm({
   form: 'quiz',
   pristine: isPristine('quiz'),
-})(Quiz)
+})(React.memo(Quiz))
 export default connect(mapStateToProps, { quesList, result })(withStyles(styles)(formValues))
